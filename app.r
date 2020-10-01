618de8116d0f2680877e742f283d7da32112d11a
Spencer Christensen <spencerjamesis@gmail.com>
2020-09-30 17:31:09 -0700

    Fixed data formatting, updated gitignore

--- app.r
+++ app.r
@@ -4,13 +4,6 @@ library(shinydashboard)
 library("googlesheets4")
 library("DT")
 
-#(deaths <- gs4_example("deaths") %>%
-#    range_read(range = "other_data", col_types = "????DD"))
-#deaths_one   <- deaths[ 1:5, ]
-#sheet_append("https://docs.google.com/spreadsheets/d/13G-tkOyOIv4XIzpRywgc2FEcIAJOr8jntjSPKGVf8cE/edit#gid=0",
-#             deaths_one,
-#             sheet = 1)
-
 ui <- tagList(
   tags$head(includeScript("navAppend.js")),
   navbarPage(
@@ -50,14 +43,10 @@ server <- function(input, output) {
   
   formData <- reactive({
     data.frame(
-      Name = c("Question 1",
-               "Question 2",
-               "Question 3"),
-      Value = as.character(c(input$question_1,
-                             input$question_2,
-                             input$question_3)
-                           ),
-      stringsAsFactors = FALSE)
+      q_1 = c(input$question_1),
+      q_2 = c(input$question_2),
+      q_3 = c(input$question_3)
+    )
   })
   
   
